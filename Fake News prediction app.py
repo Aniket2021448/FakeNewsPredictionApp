fd89c5a2de8b33775ccc7b75e1adc87d323d917b
Aniket <108399480+Aniket2021448@users.noreply.github.com>
2024-03-24 15:18:31 +0530

    basic app done

---
+++ Fake News prediction app.py
@@ -0,0 +1,65 @@
+import streamlit as st
+st.set_page_config(page_title="News Prediction", page_icon=":earth_africa:")
+from streamlit_option_menu import option_menu
+import time
+import asyncio
+import About, Home, Contact
+
+
+def render_About_page():
+    About.main()
+
+
+def render_home_page():
+    Home.main()
+
+
+def render_contact_page():
+    Contact.main()
+
+
+# TO remove streamlit branding and other running animation
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+"""
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+# Spinners
+bar = st.progress(0)
+for i in range(101):
+    bar.progress(i)
+    # time.sleep(0.02)  # Adjust the sleep time for the desired speed
+
+st.balloons()
+
+# Web content starts
+# Navbar starts
+    # Create the Streamlit app
+col1, col2 = st.columns([1, 10])
+with col1:
+    st.header("	:globe_with_meridians:")
+with col2:
+    st.header("Fake News Prediction App")
+    
+
+
+selected = option_menu(
+    menu_title="",
+    options=["Home", "About", "Contact"],
+    icons=['house', 'kanban', 'envelope'],
+    menu_icon="",
+    default_index=0,
+    orientation="horizontal",
+    styles="height: {300px;}, padding: {0px;}, margin: {0px;}, background-color: {white;}"
+)
+
+if selected == "About":
+    render_About_page()
+elif selected == "Contact":
+    render_contact_page()
+else:
+    render_home_page()
+
---
+++ Home.py
@@ -0,0 +1,58 @@
+import streamlit as st
+
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
+
+# Initialize NLTK resources
+nltk.download('stopwords')
+
+# Load the model
+news_model = pickle.load(open("fake_news_predictor_model.pkl", "rb"))
+vectorizer = pickle.load(open("fakeNews_tfidf_vectorizer.pkl", "rb"))
+
+# Function for preprocessing input text
+def preProcessing(author, title, text):
+    input_corpus = title + " " + author + " " + text
+    input_corpus = re.sub('[^a-zA-Z]', ' ', input_corpus)
+    input_corpus = input_corpus.lower()
+    input_corpus = input_corpus.split()
+    ps = PorterStemmer()
+    input_corpus = [ps.stem(word) for word in input_corpus if not word in set(stopwords.words('english'))]
+    input_corpus = ' '.join(input_corpus)
+    return input_corpus
+
+# Function to convert text into numerical vector using TF-IDF
+def convertIntoVector(X):
+    # Now converting the textual data into numerical vectors using the initialized TF-IDF vectorizer
+    X = vectorizer.transform(X)
+    return X
+
+def main():
+        
+
+    # Create the form
+    with st.form("news_form"):
+        st.subheader("Enter News Details")
+        author = st.text_input("Author Name")
+        title = st.text_input("Title")
+        text = st.text_area("Text")
+        submit_button = st.form_submit_button("Submit")
+
+    # Process form submission and make prediction
+    if submit_button:
+
+        input_text = preProcessing(author, title, text) 
+        numerical_data = convertIntoVector([input_text])
+        prediction = news_model.predict(numerical_data)
+        
+        st.subheader(":loudspeaker:Prediction:")
+        st.write(prediction)
+        if prediction[0] == 0:
+            st.write("This news is predicted to be **real**.:muscle:")
+        else:
+            st.write("This news is predicted to be **fake**.:shit:")
+
